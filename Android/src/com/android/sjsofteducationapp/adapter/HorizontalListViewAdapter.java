f74d8defafff869ef241ee0443516861409fbb86
ToanNM <gs.hbsky@gmail.com>
2015-10-08 17:20:00 +0700

    Update missing file

--- Android/src/com/android/sjsofteducationapp/adapter/HorizontalListViewAdapter.java
+++ Android/src/com/android/sjsofteducationapp/adapter/HorizontalListViewAdapter.java
@@ -3,22 +3,16 @@ package com.android.sjsofteducationapp.adapter;
 import java.util.ArrayList;
 
 import com.android.sjsofteducationapp.R;
-import com.android.sjsofteducationapp.SubjectActivity;
 import com.android.sjsofteducationapp.model.Home;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class HorizontalListViewAdapter extends ArrayAdapter<Home> {
 	private Context context;
@@ -54,7 +48,6 @@ public class HorizontalListViewAdapter extends ArrayAdapter<Home> {
 			convertView = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(resource,
 					parent, false);
 			holder = new ViewHolder();
-			holder.main = (LinearLayout) convertView.findViewById(R.id.main);
 			holder.image = (ImageView) convertView.findViewById(R.id.image);
 			holder.title = (TextView) convertView.findViewById(R.id.title);
 
@@ -72,21 +65,6 @@ public class HorizontalListViewAdapter extends ArrayAdapter<Home> {
 			holder.title.setText(title);
 		} catch (Exception e) {
 		}
-		
-		holder.main.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				holder.main.startAnimation(AnimationUtils.loadAnimation(context,
-						R.anim.abc_fade_in));
-				Toast.makeText(context, title, Toast.LENGTH_SHORT).show();
-				if (title.equalsIgnoreCase("Animals")) {
-					Intent intent = new Intent(context, SubjectActivity.class);
-					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-					context.startActivity(intent);
-				}
-			}
-		});
 
 		return convertView;
 	}
@@ -94,7 +72,6 @@ public class HorizontalListViewAdapter extends ArrayAdapter<Home> {
 	private class ViewHolder {
 		ImageView image;
 		TextView title;
-		LinearLayout main;
 	}
 
 }
